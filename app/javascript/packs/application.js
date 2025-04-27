7b77f43d14aa77d7fbdae527c653eb7470af99bc
AKogi0718 <51438300+AKogi0718@users.noreply.github.com>
2025-04-27 19:17:04 +0900

    deploy

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,12 @@
+// This file is automatically compiled by Webpack
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+
+// Import Rails UJS
+// import Rails from "@rails/ujs"
+// Rails.start()
+
+console.log('Hello from application.js')
