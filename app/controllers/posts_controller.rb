69f90dc85efdbf7cd1d3cd92a59d8c8fb3d0fba3
AKogi0718 <51438300+AKogi0718@users.noreply.github.com>
2025-06-15 17:32:45 +0900

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,4 +12,23 @@ class PostsController < ApplicationController
     month = params[:month]
     redirect_to theaccounting_path(year: year, month: month)
   end
+  def theaccounting
+    @year = params[:year]
+    @month = params[:month]
+
+    # 外部DB（prosper）からのデータ取得
+    @posts = ProsperPost.where("EXTRACT(YEAR FROM date) = ? AND EXTRACT(MONTH FROM date) = ?", @year, @month)
+    @comp = ProsperCorporation.all
+    @urikake1 = ProsperUrikake.where(process: 1)
+    @urikake2 = ProsperUrikake.where(process: 2)
+    @pastprice = ProsperPost.where(process: 3)
+    @nowprice = ProsperPost.where(process: 4)
+    @pastallnyukin = ProsperPost.where(process: 5)
+    @nowallnyukin = ProsperPost.where(process: 6)
+    @pastallsousai = ProsperPost.where(process: 7)
+    @nowsousai = ProsperPost.where(process: 8)
+
+    render 'theaccounting'
+  end
+
 end
