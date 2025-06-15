a39b21b8108ca6ad15fac938ae9b059601ee587a
AKogi0718 <51438300+AKogi0718@users.noreply.github.com>
2025-06-15 17:26:11 +0900

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,6 @@
-class CategoriesController < ApplicationController
+# app/controllers/posts_controller.rb
+class PostsController < ApplicationController
   def index
-  end
-
-  def show
-  end
-
-  def new
-  end
-
-  def edit
-  end
-
-  def create
-  end
-
-  def update
-  end
-
-  def destroy
+    render plain: "Hello from Posts#index"
   end
 end
