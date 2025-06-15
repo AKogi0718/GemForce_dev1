26bcbd4e3a76507413f87e501980639e63323268
AKogi0718 <51438300+AKogi0718@users.noreply.github.com>
2025-06-15 17:29:58 +0900

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,13 @@ class PostsController < ApplicationController
   def index
     render plain: "Hello from Posts#index"
   end
+
+  def select
+  end
+
+  def monthselect
+    year = params[:year]
+    month = params[:month]
+    redirect_to theaccounting_path(year: year, month: month)
+  end
 end
